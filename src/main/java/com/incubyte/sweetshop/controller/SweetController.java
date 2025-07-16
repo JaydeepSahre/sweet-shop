e75880c6c8b39ef883d38c509610893f765a60e1
JaydeepSahre <sahrejaydeep05@gmail.com>
2025-07-16 18:31:16 +0530

    feat: add GET /sweets endpoint to fetch all sweets from SweetService

---
+++ src/main/java/com/incubyte/sweetshop/controller/SweetController.java
@@ -0,0 +1,25 @@
+package com.incubyte.sweetshop.controller;
+
+import com.incubyte.sweetshop.model.Sweet;
+import com.incubyte.sweetshop.repository.InMemorySweetRepository;
+import com.incubyte.sweetshop.service.SweetService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sweets")
+public class SweetController {
+
+    private final SweetService sweetService;
+
+    public SweetController() {
+        // In-memory repository setup for now
+        this.sweetService = new SweetService(new InMemorySweetRepository());
+    }
+
+    @GetMapping
+    public List<Sweet> getAllSweets() {
+        return sweetService.getAllSweets();
+    }
+}
